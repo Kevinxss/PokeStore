701d7f21122d01c21e591327058f1ada9bb33819
Kevinxss <99285547+Kevinxss@users.noreply.github.com>
2025-10-02 23:36:04 -0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,5 @@
 <?php
-// Archivo principal de la API
-// Aquí puedes incluir el router principal o el punto de entrada para manejar las peticiones
+
 
 // Redirección automática al login
 header('Location: public/index.html');
